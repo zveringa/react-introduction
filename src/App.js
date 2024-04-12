c6309617747c838217c8aea06a82ede386601566
Eugene Zveringa <zveringa@gmail.com>
2024-04-12 23:33:07 +0100

    without JSX

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import { startTransition } from 'react';
+import { createElement } from 'react';
 
 export const App = () => {
-	const currentYear = new Date().getFullYear(); //{`пример Императивного подхода`}
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-			<footer>
-				{/* пример декларативного подхода */}
-				<p>&copy; {currentYear} </p>
-			</footer>
-		</div>
+	const currentYear = new Date().getFullYear();
+
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+		),
+		createElement('footer', null, createElement('p', null, currentYear)),
 	);
 };
